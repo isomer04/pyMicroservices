8e2b767fa5adb94b0120c81acf75776fd6dbcef3
isomer04 <43922158+isomer04@users.noreply.github.com>
2024-01-30 10:47:18 -0500

    Added MySQL configuration in the settings file

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'admin',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
